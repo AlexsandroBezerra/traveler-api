6a725578f03293214371221a7615a4254286b8b6
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-01-25 20:02:58 -0300

    feat: add typeorm

---
+++ ormconfig.js
@@ -0,0 +1,33 @@
+require('dotenv/config')
+
+const devConfig = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB_NAME,
+  entities: ['./src/**/infra/database/entities/*.ts'],
+  migrations: ['./src/infra/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/infra/database/migrations'
+  }
+}
+
+const prodConfig = {
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB_NAME,
+  entities: ['./dist/**/infra/database/entities/*.js'],
+  migrations: ['./dist/infra/database/migrations/*.js'],
+  cli: {
+    migrationsDir: './src/shared/infra/typeorm/migrations'
+  }
+}
+
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig
+
+module.exports = config
