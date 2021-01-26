521f2fd599ef85b572e74804d824b0126309dbcf
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-01-25 22:23:50 -0300

    chore: add docker compose

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,7 @@ require('dotenv/config')
 const devConfig = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
+  port: process.env.POSTGRES_PORT || 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB_NAME,
@@ -17,7 +17,7 @@ const devConfig = {
 const prodConfig = {
   type: 'postgres',
   host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
+  port: process.env.POSTGRES_PORT || 5432,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB_NAME,
