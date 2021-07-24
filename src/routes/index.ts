deb8d87101be3de66958787fdd8347d29290178f
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-07-24 16:56:57 -0300

    feat: Generate JWT token

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { citiesRouter } from "./cities.routes";
+import { sessionsRouter } from "./sessions.routes";
 import { usersRouter } from "./users.routes";
 
 export const routes = Router();
 
 routes.use("/cities", citiesRouter);
 routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,40 @@
+import bcrypt from "bcryptjs";
+import { Router } from "express";
+import { getRepository } from "typeorm";
+import jwt from "jsonwebtoken";
+
+import { User } from "../models/User";
+import { authConfigs } from "../configs/auth";
+
+export const sessionsRouter = Router();
+
+const invalidCredentialsResponse = {
+  error: true,
+  code: "invalid.credentials",
+  message: "Invalid email/password combination",
+}
+
+sessionsRouter.post("/", async (request, response) => {
+  const { email, password } = request.body;
+
+  const usersRepository = getRepository(User);
+
+  const user = await usersRepository.findOne({ email });
+
+  if (!user) {
+    return response.status(400).json(invalidCredentialsResponse);
+  }
+
+  const hashCompare = await bcrypt.compare(password, user.password);
+
+  if (!hashCompare) {
+    return response.status(400).json(invalidCredentialsResponse);
+  }
+
+  const token = jwt.sign({}, authConfigs.jwt.secret, {
+    subject: user.id.toString(),
+    expiresIn: authConfigs.jwt.expiresIn,
+  });
+
+  return response.json({ token });
+});
