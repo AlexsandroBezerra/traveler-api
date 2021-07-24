3fedcd052d3d9b3c1b8451fded63ddee518c2934
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-07-24 10:38:10 -0300

    feat: Add express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+import express from "express";
+
+const PORT = 3333;
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello, world!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
